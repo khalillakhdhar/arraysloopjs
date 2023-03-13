1f50437ff501705da68f16993e3d2a3a2a6d702c
khalil <khalillakhdharatc@gmail.com>
2023-03-13 10:53:46 +0100

    Initial commit

---
+++ forExemple.js
@@ -0,0 +1,14 @@
+let fact=1;
+for (var i = 2; i <= 5; i++) {
+    fact=fact*i;
+    //iteration 1= fact=1 i=2=> fact=2
+    //iteration 2= fact=2 i=3=> fact=6
+    //...
+
+    }
+    a=5; b=10;
+    for(let i=a;i<=b;i++) {
+    if(i%2==0) {
+        console.log("i= "+i);
+    }
+    }
